5ea376347d45e4d4e2b481c6c85fe8749c5b9657
rodrificarra <roodrificarra@gmail.com>
2023-03-23 20:01:13 -0300

    creando navbar

---
+++ ecommerce-rodri/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h3>Ecommerce</h3>
+      <div>
+        <button>PCS Armadas</button>
+        <button>Placas de Video</button>
+        <button>Procesadores</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
